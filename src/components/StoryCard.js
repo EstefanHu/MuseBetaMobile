0ecaee297c1713a748d5b94cdf73190db57f86d9
Estefan Hu <estefanhu074@gmail.com>
2020-06-30 16:42:06 -0700

    starting button routing

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -3,12 +3,15 @@ import {
   StyleSheet,
   View,
   Text,
-  TouchableOpacity
+  TouchableOpacity,
+  Image
 } from 'react-native';
 import {
   Ionicons,
   FontAwesome
 } from '@expo/vector-icons';
+
+import userDefault from './../../assets/user-default.png';
 
 const styles = StyleSheet.create({
   card: {
@@ -19,6 +22,31 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+    marginBottom: 5
+  },
+  meta: {
+    color: 'grey',
+    flexDirection: 'row'
+  },
+  profileImg: {
+    height: 30,
+    width: 30,
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    borderRadius: 15,
+    marginRight: 10
+  },
+  author: {
+    fontWeight: 'bold',
+    fontSize: 13,
+  },
+  geolocation: {
+    fontSize: 13,
+  },
+  prime: {
     paddingHorizontal: 15,
     flexDirection: 'row',
     justifyContent: 'space-between'
@@ -27,14 +55,14 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 20
   },
+  pitch: {
+    paddingHorizontal: 15,
+    fontSize: 16,
+  },
   metaContainer: {
+    paddingHorizontal: 15,
     flexDirection: 'row',
     marginVertical: 5,
-  },
-  meta: {
-    paddingHorizontal: 15,
-    color: 'grey',
-    fontSize: 13,
   },
   actions: {
     flexDirection: 'row',
@@ -57,30 +85,51 @@ const styles = StyleSheet.create({
 export const StoryCard = ({ navigation, item }) => {
   return (
     <View style={styles.card}>
+
       <View style={styles.header}>
+        <View style={styles.meta}>
+          <Image
+            style={styles.profileImg}
+            source={userDefault}
+          />
+          <View>
+            <TouchableOpacity>
+              <Text style={styles.author}>{item.authorName}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.geolocation}>{item.city}</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <TouchableOpacity>
+          <Text>...</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.prime}>
         <Text style={styles.title}>{item.title}</Text>
         <Text>{item.genre}</Text>
       </View>
-      <Text>{item.pitch}</Text>
+
+      <Text style={styles.pitch}>{item.pitch}</Text>
+
       <View style={styles.metaContainer}>
-        <TouchableOpacity>
-          <Text style={styles.meta}>{item.authorName}</Text>
-        </TouchableOpacity>
-        <Text style={styles.meta}> - {item.createdAt}</Text>
+        <Text style={styles.meta}>{Date.now()}</Text>
       </View>
+
       <View style={styles.actions}>
         <TouchableOpacity
           onPress={() => navigation.navigate('StoryDetailScreen', { _id: item._id })}
         >
           <View style={styles.button}>
-            <Ionicons name='md-book' size={20} color='grey' />
-            <Text> Read</Text>
+            <Ionicons name='md-book' size={22} color='grey' />
+            <Text>  Read</Text>
           </View>
         </TouchableOpacity>
         <TouchableOpacity>
           <View style={styles.button}>
-            <FontAwesome name='bookmark-o' size={20} color='grey' />
-            <Text> Save</Text>
+            <FontAwesome name='bookmark-o' size={22} color='grey' />
+            <Text>  Save</Text>
           </View>
         </TouchableOpacity>
       </View>
