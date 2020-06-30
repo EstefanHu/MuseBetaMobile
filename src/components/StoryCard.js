2ebe83c260fed41c618df8cea1b82260f1a60262
Estefan Hu <estefanhu074@gmail.com>
2020-06-30 14:51:26 -0700

    added button styles

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -5,17 +5,21 @@ import {
   Text,
   TouchableOpacity
 } from 'react-native';
+import {
+  Ionicons,
+  FontAwesome
+} from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
   card: {
     backgroundColor: 'white',
     width: 350,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+    paddingVertical: 15,
     borderRadius: 5,
     marginTop: 10,
   },
   header: {
+    paddingHorizontal: 15,
     flexDirection: 'row',
     justifyContent: 'space-between'
   },
@@ -23,15 +27,34 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 20
   },
-  read: {
+  metaContainer: {
+    flexDirection: 'row',
+    marginVertical: 5,
+  },
+  meta: {
+    paddingHorizontal: 15,
+    color: 'grey',
+    fontSize: 13,
+  },
+  actions: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  button: {
     backgroundColor: 'rgb(245,245,245)',
-    borderWidth: 1,
+    borderBottomWidth: 1,
     borderColor: 'lightgrey',
     borderRadius: 5,
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
 
-export const StoryCard = ({navigation, item}) => {
+export const StoryCard = ({ navigation, item }) => {
   return (
     <View style={styles.card}>
       <View style={styles.header}>
@@ -39,15 +62,25 @@ export const StoryCard = ({navigation, item}) => {
         <Text>{item.genre}</Text>
       </View>
       <Text>{item.pitch}</Text>
-      <View style={styles.meta}>
-        <Text>{item.authorName} - {item.createdAt}</Text>
+      <View style={styles.metaContainer}>
+        <TouchableOpacity>
+          <Text style={styles.meta}>{item.authorName}</Text>
+        </TouchableOpacity>
+        <Text style={styles.meta}> - {item.createdAt}</Text>
       </View>
       <View style={styles.actions}>
         <TouchableOpacity
           onPress={() => navigation.navigate('StoryDetailScreen', { _id: item._id })}
         >
-          <View style={styles.read}>
-            <Text>Read</Text>
+          <View style={styles.button}>
+            <Ionicons name='md-book' size={20} color='grey' />
+            <Text> Read</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.button}>
+            <FontAwesome name='bookmark-o' size={20} color='grey' />
+            <Text> Save</Text>
           </View>
         </TouchableOpacity>
       </View>
