e60f240295f0247ee8648bbecff61527b818a497
Estefan Hu <estefanhu074@gmail.com>
2020-07-09 00:42:33 -0700

    added city welcome

--- src/screens/welcome/WelcomeLaunchScreen.js
+++ src/screens/welcome/WelcomeLaunchScreen.js
@@ -7,6 +7,7 @@ import {
 } from 'react-native';
 
 import { Context as AuthContext } from './../../providers/AuthProvider.js';
+import { Context as LocationContext } from './../../providers/LocationProvider.js';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,6 +15,20 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     alignItems: 'center',
     backgroundColor: 'rgb(255,50,50)',
+  },
+  heroContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  hero: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: 'white',
   },
   next: {
     backgroundColor: 'white',
@@ -32,6 +47,7 @@ const styles = StyleSheet.create({
 
 export const WelcomeLaunchScreen = ({ navigation }) => {
   const { finishTutorial } = useContext(AuthContext);
+  const { state: { city } } = useContext(LocationContext);
 
   const toApplication = () => {
     finishTutorial();
@@ -40,6 +56,11 @@ export const WelcomeLaunchScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.heroContainer}>
+        <Text style={styles.welcome}>Welcome to</Text>
+        <Text style={styles.hero}>{city}'s</Text>
+        <Text style={styles.welcome}>story</Text>
+      </View>
       <TouchableOpacity onPress={toApplication}>
         <View style={styles.next}>
           <Text style={styles.nextText}>Agree</Text>
