681a66d65ad7300610d16d00f4d7c107fcc7136c
Estefan Hu <estefanhu074@gmail.com>
2020-07-14 21:41:52 -0700

    installed header

--- src/screens/journey/JourneyHomeScreen.js
+++ src/screens/journey/JourneyHomeScreen.js
@@ -10,6 +10,7 @@ import {
 } from 'react-native';
 import { useScrollToTop } from '@react-navigation/native';
 import MapView, { Marker } from 'react-native-maps';
+import { Ionicons } from '@expo/vector-icons';
 
 import { Context as JourneyContext } from './../../providers/JourneyProvider.js';
 import { Context as StoryContext } from './../../providers/StoryProvider.js';
@@ -28,6 +29,7 @@ const styles = StyleSheet.create({
 export const JourneyHomeScreen = ({ navigation }) => {
   const { state: { stories }, fetchNearStories } = useContext(StoryContext);
   const { state: { longitude, latitude }, getCoords } = useContext(LocationContext);
+  const { state: { storyId } } = useContext(JourneyContext);
   const [refreshing, setRefreshing] = useState(false);
   const [channel, setChannel] = useState('All');
 
@@ -39,7 +41,6 @@ export const JourneyHomeScreen = ({ navigation }) => {
 
   const scroll = React.useRef(null);
   useScrollToTop(scroll);
-
 
   const onRefresh = async () => {
     setRefreshing(true);
@@ -62,11 +63,18 @@ export const JourneyHomeScreen = ({ navigation }) => {
         keyExtractor={item => item._id}
         showsVerticalScrollIndicator={false}
         ListHeaderComponent={() =>
-          <Header
-            stories={stories}
-            longitude={longitude}
-            latitude={latitude}
-          />
+          <>
+            {
+              storyId ?
+                <LaunchPad
+                  stories={stories}
+                  longitude={longitude}
+                  latitude={latitude}
+                />
+                : <NoStory />
+            }
+            <Text style={launchPadStyles.hero}>Stories around you</Text>
+          </>
         }
         renderItem={({ item }) => {
           return item.channel === channel
@@ -81,7 +89,7 @@ export const JourneyHomeScreen = ({ navigation }) => {
   );
 };
 
-const headerStyles = StyleSheet.create({
+const launchPadStyles = StyleSheet.create({
   launcher: {
     backgroundColor: 'white',
     width: Dimensions.get('window').width - 20,
@@ -89,12 +97,6 @@ const headerStyles = StyleSheet.create({
     paddingHorizontal: 15,
     marginTop: 10,
     borderRadius: 5,
-  },
-  launcherHero: {
-    color: 'grey',
-    fontWeight: 'bold',
-    fontSize: 20,
-    marginTop: 10
   },
   mapStyle: {
     height: 150,
@@ -130,9 +132,11 @@ const headerStyles = StyleSheet.create({
   },
 })
 
-const Header = ({ stories, longitude, latitude }) => {
-  const { state: { status, storyId, story }, fetchJourney } = useContext(JourneyContext);
-  const [dockedStory, setDockedStory] = useState();
+const LaunchPad = ({ stories, longitude, latitude }) => {
+  const { state: { storyId, story }, fetchJourney } = useContext(JourneyContext);
+  const [id, setId] = useState();
+  const [title, setTitle] = useState('');
+  const [coordinates, setCoordinates] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -148,22 +152,24 @@ const Header = ({ stories, longitude, latitude }) => {
             Math.floor(stories.length)
           )];
       }
-      setDockedStory(displayStory);
+      setId(displayStory._id);
+      setTitle(displayStory.title);
+      setCoordinates(displayStory.startLocation.coordinates);
     })();
   }, [stories, storyId]);
 
   useEffect(() => {
-    if (previewMap.current && dockedStory)
+    if (previewMap.current && coordinates)
       fitMarkers();
-  }, [dockedStory, previewMap]);
+  }, [coordinates, previewMap]);
 
   const previewMap = React.useRef(null);
 
   const fitMarkers = () => {
     const MARKERS = [
       {
-        latitude: dockedStory.startLocation.coordinates[1],
-        longitude: dockedStory.startLocation.coordinates[0]
+        latitude: coordinates[1],
+        longitude: coordinates[0]
       },
       { latitude: latitude, longitude: longitude }
     ]
@@ -173,54 +179,102 @@ const Header = ({ stories, longitude, latitude }) => {
         right: 60,
         bottom: 30,
         left: 60
-      },
-      // animated: false
+      }
     }
     previewMap.current.fitToCoordinates(MARKERS, OPTIONS);
   }
 
   return (
-    <>
-      <Text style={headerStyles.launcherHero}>
-        {status === 'docked' ? 'Story loaded!' : 'No story loaded.'}
-      </Text>
-      {
-        dockedStory ?
-          <View style={headerStyles.launcher}>
-            <Text>Launch recommendation?</Text>
-            <Text>{dockedStory.title}</Text>
-            <MapView
-              style={headerStyles.mapStyle}
-              ref={previewMap}
-              initialRegion={{
-                longitude: longitude,
-                latitude: latitude,
-                longitudeDelta: 0.1,
-                latitudeDelta: 0.1
-              }}
-              showsUserLocation
-              scrollEnabled={false}
-            >
-              <Marker
-                coordinate={{
-                  latitude: dockedStory.startLocation.coordinates[1],
-                  longitude: dockedStory.startLocation.coordinates[0]
-                }}
-              />
-            </MapView>
-            <TouchableOpacity
-              style={headerStyles.launchButton}
-              onPress={() => navigation.navigate(
-                'JourneyLaunchScreen',
-                { story: dockedStory }
-              )}
-            >
-              <Text style={headerStyles.launchButtonText}>Launch</Text>
-            </TouchableOpacity>
-          </View>
-          : <View style={headerStyles.placeholder} />
-      }
-      <Text style={headerStyles.hero}>Stories around you</Text>
-    </>
+    <View style={launchPadStyles.launcher} >
+      <Text>{title}</Text>
+      <MapView
+        style={launchPadStyles.mapStyle}
+        ref={previewMap}
+        initialRegion={{
+          longitude: longitude,
+          latitude: latitude,
+          longitudeDelta: 0.1,
+          latitudeDelta: 0.1
+        }}
+        showsUserLocation
+        scrollEnabled={false}
+      >
+        <Marker
+          coordinate={{
+            latitude: coordinates[1],
+            longitude: coordinates[0]
+          }}
+        />
+      </MapView>
+      <TouchableOpacity
+        style={launchPadStyles.launchButton}
+        onPress={() => navigation.navigate('JourneyLaunchScreen', { id })}
+      >
+        <Text style={launchPadStyles.launchButtonText}>Launch</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const noStoryStyles = StyleSheet.create({
+  launcher: {
+    backgroundColor: 'white',
+    width: Dimensions.get('window').width - 20,
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    marginTop: 10,
+    borderRadius: 5,
+  },
+  noStoryContainer: {
+    flexDirection: 'row',
+  },
+  noStoryText: {
+    paddingLeft: 40,
+    paddingTop: 5,
+    alignItems: 'center',
+  },
+  noStoryHero: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 5
+  },
+  instructions: {
+    fontSize: 17,
+  },
+  launchButton: {
+    backgroundColor: 'grey',
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 8,
+  },
+  launchButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
+
+const NoStory = () => {
+  const getRecommendation = () => {
+    console.log('recommendation');
+  }
+
+  return (
+    <View style={noStoryStyles.launcher}>
+      <View style={noStoryStyles.noStoryContainer}>
+        <Ionicons name='ios-navigate' size={90} color='lightgrey' />
+        <View style={noStoryStyles.noStoryText}>
+          <Text style={noStoryStyles.noStoryHero}>No story loaded.</Text>
+          <Text style={noStoryStyles.instructions}>Find a story and click {'\n'} 'Read' to load a story.</Text>
+        </View>
+      </View>
+      <TouchableOpacity
+        style={noStoryStyles.launchButton}
+        onPress={getRecommendation}
+      >
+        <Text style={noStoryStyles.launchButtonText}>Get Recommendation</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
