bd10ae5ff47c7aa6886cea2961b757fccde1edac
Estefan Hu <estefanhu074@gmail.com>
2020-07-15 15:57:38 -0700

    added initial rendering

--- src/screens/journey/JourneyLaunchScreen.js
+++ src/screens/journey/JourneyLaunchScreen.js
@@ -3,21 +3,36 @@ import {
   StyleSheet,
   View,
   Text,
-  Permissions,
-  Linking
+  Dimensions,
 } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
 import { Context as JourneyContext } from './../../providers/JourneyProvider.js';
+import { Context as LocationContext } from './../../providers/LocationProvider.js';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: '100%',
+  },
 });
 
-export const JourneyLaunchScreen = () => {
+export const JourneyLaunchScreen = ({ route, navigation }) => {
+  const initialRegion = React.useRef(region);
   const { state: { status }, fetchJourney } = useContext(JourneyContext);
+  const { state: { longitude, latitude } } = useContext(LocationContext);
+  const [region, setRegion] = useState({
+    longitude: longitude,
+    latitude: latitude,
+    longitudeDelta: 0.1,
+    latitudeDelta: 0.1
+  });
+  const story = route.params?.story;
 
   useEffect(() => {
 
@@ -25,7 +40,27 @@ export const JourneyLaunchScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text>Launch</Text>
+      <MapView
+        style={styles.mapStyle}
+        region={region}
+        onRegionChangeComplete={setRegion}
+        mapType={"mutedStandard"}
+        showsScale
+        showsIndoors
+        loadingEnabled
+        compassOffset={{ x: -5, y: 5 }}
+        showsUserLocation
+        showsMyLocationButton
+      >
+        <Marker
+          coordinate={{
+            latitude: story.startLocation.coordinates[1],
+            longitude: story.startLocation.coordinates[0]
+          }}
+        >
+
+        </Marker>
+      </MapView>
     </View>
   );
 };
