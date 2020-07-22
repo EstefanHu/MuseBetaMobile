96ce80b12bc583e51ee67b09624bfb3d5d572909
Estefan Hu <estefanhu074@gmail.com>
2020-07-21 19:24:08 -0700

    added base search methods

--- src/providers/SearchProvider.js
+++ src/providers/SearchProvider.js
@@ -1,16 +1,33 @@
 import createDataContext from './createDataContext.js';
 
-const SearchReducer = (state, action) => {
-  switch(action.type) {
-    default: 
-    return state;
+const searchReducer = (state, action) => {
+  switch (action.type) {
+    case 'update_query':
+      return { ...state, query: action.payload }
+    case 'cancel_query':
+      return { ...state, query: null }
+    default:
+      return state;
   };
 };
+
+const updateQuery = dispatch => async query => {
+  try {
+    dispatch({ type: 'update_query', payload: query });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const cancelQuery = dispatch => () => dispatch({ type: 'cancel_query' });
 
 export const { Context, Provider } = createDataContext(
   searchReducer,
   {
+    updateQuery,
+    cancelQuery
   },
   {
+    query: null
   }
 );
